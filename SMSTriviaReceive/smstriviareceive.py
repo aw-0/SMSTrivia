0595cb48020c8a20ce9230defe85353f81f85a47
andrewwolf0 <43796708+andrewwolf0@users.noreply.github.com>
2020-05-24 12:08:17 -0500

    Added Question Tracking
    -Added Question Tracking (currentQuesttion)
    -Score Tracking (totalScore)
    -Answer Tracking
    -Logic to check if answer is correct

--- SMSTriviaReceive/smstriviareceive.py
+++ SMSTriviaReceive/smstriviareceive.py
@@ -6,6 +6,28 @@ import twilio_config as tcfg, qna_settings as qaset, players_config as pcfg
 account_sid = tcfg.twilio["account_sid"]
 auth_token = tcfg.twilio["auth_token"]
 client = Client(account_sid, auth_token)
+phones = pcfg.phones
+
+player_answers = {
+    pcfg.phones[0]: {
+            "totalScore": 0,
+            "currentQuestion": 1,
+            "answers": {
+            "1": "",
+            "2": "",
+            "3": "",
+            "4": "",
+            "5": ""}},
+    pcfg.phones[1]: {
+            "totalScore": 0,
+            "currentQuestion": 1,
+            "answers": {
+            "1": "",
+            "2": "",
+            "3": "",
+            "4": "",
+            "5": ""}}}
+print(player_answers["+18479221678"]["currentQuestion"])
 
 app = Flask(__name__)
 
@@ -37,18 +59,28 @@ def sms():
     number = request.form['From']
     message_body = request.form['Body']
 
-    lastq,lasta = message_body.split(":")
-    nextkey = str(int(lastq) + 1)
+    # lastq,lasta = message_body.split(":")
+    # nextkey = str(int(lastq) + 1)
 
-    #if message_body == "1:D":
-    file_object = open('log.txt', 'a')
-    file_object.write("\n Correct Answer")
-    file_object.close()
+    lasta = message_body
+    currentQuestion = player_answers[number]["currentQuestion"]
+
+    print(qaset.answers[str(currentQuestion)]["correct"])
+    print(lasta)
+    if lasta == qaset.answers[str(currentQuestion)]["correct"]:
+        score = player_answers[number]["totalScore"]
+        score += 1
+        player_answers[number]["totalScore"] = score
+        file_object = open('log.txt', 'a')
+        file_object.write(f"{number}, {currentQuestion}, {str(score)}")
+        file_object.close()
+
+    player_answers[number]["currentQuestion"] += 1
+    nextkey = str(player_answers[number]["currentQuestion"])
 
     nextq = send_question(number, qaset.questions, qaset.answers, nextkey)
 
 if __name__ == '__main__':
-    phones = pcfg.phones
     for number in phones:
         # sends first question to all
         qa = send_question(number, qaset.questions, qaset.answers, "1")
